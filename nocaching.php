a74740f3c542c80592ae515c5b507172c88b2e09
diogomene <53102635+diogomene@users.noreply.github.com>
2020-11-05 23:39:15 -0400

    Add files via upload
    -Added prototype of php, for no caching in client;
    -Added CSS file with the older styles;
    -Changed somethings on the verification, changing the script tag at the bottom of the document:

---
+++ nocaching.php
@@ -0,0 +1,7 @@
+<?php
+    header("Cache-Control: no-cache, must-revalidate");
+    header("Pragma: no-cache");
+    header("Expires: Wed, 1 Jan 2020 00:00:00 GMT");
+?>
+var triot="t";
+console.log(triot);
